1369c7f03f1f9f9d773d309532f50dc040636048
Greg Van Buren <g.vanburen@gmail.com>
2015-06-02 07:17:48 -0700

    Cleaned twitter handle

--- public/components/tweets/tweets.controller.js
+++ public/components/tweets/tweets.controller.js
@@ -1,35 +1,33 @@
 'use strict';
 
 angular.module('textAnalysis')
-	.config(function($httpProvider) {
-  		$httpProvider.defaults.useXDomain = true;
-  		delete $httpProvider.defaults.headers.common['X-Requested-With'];
-		$httpProvider.defaults.headers.common['Accept'] = 'application/json, text/javascript';
+	.config(function($httpProvider){
+		$httpProvider.defaults.useXDomain = true;
+		$httpProvider.defaults.withCredentials = true;
+		delete $httpProvider.defaults.headers.common["X-Requested-With"];
+		$httpProvider.defaults.headers.common["Accept"] = "application/json";
+		$httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 	})
-	.controller('tweetCtrl', ['$rootScope', '$scope', '$http', 'twitterService', 
-		function($rootScope, $scope, $http, twitterService) {
+	.controller('tweetCtrl', ['$routeParams','$scope', '$http', 'twitterService', 
+		function($routeParams, $scope, $http, twitterService) {
 			
-			$scope.userInput = $rootScope.userInput;
+			$scope.twitterHandle = $routeParams.handle;
 			
 			$scope.tweets = ['first', 'second', 'third', 'fourth'];
 
 			//need to figure out how to get with authorization
 			//or use the node.js SDK 
 			$scope.analyze = function(text){
-				var request = {
-					text: text,
-					application_id: "3b3a6ee8",
-					application_key: "c20b9d30b75765b73f3012dcd6fca5e2",
-					callback: "jsonp"
-				};
-				var url = "http://api.aylien.com/api/v1/sentiment?";
+				// application_id: "3b3a6ee8",
+				// application_key: "c20b9d30b75765b73f3012dcd6fca5e2",
+				var url = "http://api.aylien.com/api/v1/sentiment?text=" + text;
 				$http({
-					method: "GET",
+					method: "JSONP",
 					url: url,
+					callback: "JSON_CALLBACK",
 					headers: {
-						'Accept': 'application/json',
-					},
-					params: request
+						'Authorization': '3b3a6ee8:c20b9d30b75765b73f3012dcd6fca5e2',
+					}
 				});
 				console.log(text);
 			};
