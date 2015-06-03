a257c5b32cb19d7ffe7bea51ed2fa5133a0220e6
Asa Toburen <atopettit@gmail.com>
2015-06-03 12:46:01 -0700

    controller changes

--- app/controllers/aylien.controller.js
+++ app/controllers/aylien.controller.js
@@ -5,68 +5,74 @@ var join = Promise.join;
 
 //send text to aylien api for sentiment analysis
 
+
 var sentiment = function(req, res) {
-    aylien.sentiment({
-        'text': req.params.text
-    }).then(function(data) {
-        console.log(data);
-    }).error(function(e) {
-        console.log('Error: ', e);
+    return aylien.sentiment({
+        "text": req.params.text
+    }).then(function(data){
+      console.log(data);
     });
 };
 
-//suggests hashtags based on submitted text.
-
-var getHashtags = function(req, res) {
-    aylien.hashtags({
-        'text': req.params.text
-    }).then(function(data) {
-        console.log(data);
-    }).error(function(e) {
-        console.log('Error: ', e);
-    });
-};
-
-// Extracts entities from submitted text. URLs, names, emails.
-
-var getEntities = function(req, res) {
-    aylien.entities({
-        'text': req.params.text
-    }).then(function(data) {
-        console.log(data);
-    }).error(function(e) {
-        console.log('Error: ', e);
-    });
-};
-
-//extracts concepts from text
-
-var getConcepts = function(req, res) {
-    aylien.concepts({
-        'text': req.params.text
-    }).then(function(data) {
-        console.log(data);
-    }).error(function(e) {
-        console.log('Error: ', e);
-    });
-};
-
-
-//classifies content according to NewsCode standard
-
-var classify = function(req, res) {
-    aylien.classify({
-        'text': req.params.text
-    }).then(function(data) {
-        console.log(data);
-    }).error(function(e) {
-        console.log('Error: ', e);
-    });
-};
+////suggests hashtags based on submitted text.
+//
+//var getHashtags = function(req, res) {
+//    return new Promise(function(resolve, reject) {
+//        aylien.hashtags({
+//            'text': req.params.text
+//        }).then(function(data) {
+//            resolve(data);
+//        }).error(function(e) {
+//            reject(e);
+//        });
+//    });
+//};
+//
+//// Extracts entities from submitted text. URLs, names, emails.
+//
+//var getEntities = function(req, res) {
+//    return new Promise(function(resolve, reject) {
+//        aylien.entities({
+//            'text': req.params.text
+//        }).then(function(data) {
+//            resolve(data);
+//        }).error(function(e) {
+//            reject(e);
+//        });
+//    });
+//};
+//
+////extracts concepts from text
+//
+//var getConcepts = function(req, res) {
+//    return new Promise(function(resolve, reject) {
+//        aylien.concepts({
+//            'text': req.params.text
+//        }).then(function(data) {
+//            resolve(data);
+//        }).error(function(e) {
+//            reject(e);
+//        });
+//    });
+//};
+//
+////classifies content according to NewsCode standard
+//
+//var classify = function(req, res) {
+//    return new Promise(function(resolve, reject) {
+//        aylien.classify({
+//            'text': req.params.text
+//        }).then(function(data) {
+//            resolve(data);
+//        }).error(function(e) {
+//            reject(e);
+//        });
+//    });
+//};
 
 module.exports.getSentiment = function(req, res) {
-    join(sentiment(req), getHashtags(req), getEntities(req), getConcepts(req), classify(req),
-        function(sentiment, hashtags, entities, concepts, classes) {
-            console.log(sentiment + hashtags + concepts);
+    join(sentiment(req, res), // getHashtags(req, res), getEntities(req, res), //getConcepts(req), classify(req),
+        function(sentiment) {
+            console.log(sentiment);
         });
 };
