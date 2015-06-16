7ac509a61cb47eab98d6a97113e5537ce44a543d
Greg Van Buren <g.vanburen@gmail.com>
2015-06-15 19:02:48 -0700

    E2E testing started

--- public/e2e/textAnalysis.js
+++ public/e2e/textAnalysis.js
@@ -1,9 +1,22 @@
 'use strict';
 
-var ROOT = "http://localhost:8080";
+//var ROOT = "http://localhost:8080";
 
 describe('home page tests', function() {
+	var ROOT = "http://localhost:8080";
 
+	it('should input twitter handle', function(){
+		browser.get(ROOT + '/');
+		expect(browser.getTitle()).toEqual('Tweet Analyzer');
+		element(by.id('handle')).sendKeys('gvanburen');
+		var radioButton = element(by.css('.orange div div div form div input'))
+		browser.driver.actions().mouseMove(radioButton).perform();
+		element.all(radioButton).then(function(elm){
+			elm[0].click();
+		});
+		browser.debugger();
+		element(by.buttonText('Analyze')).click();
+	});
 
 
     //xit('should display the homepage with links and form input', function() {
